c042158372f0717aac6ed9120805861770fbd5aa
archit kumar <archit@wpi.edu>
2023-03-12 14:05:24 -0500

    Use regex to mine data
    Using fixed number offsets to locate text in pdf is not dynamic

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import re
 
 
 load_dotenv()
-
 pdf_dir = os.environ.get("pdf_loc")
 pdf_filenames = os.listdir(pdf_dir)
 df_columns = {"statement_date": None,
@@ -14,41 +14,74 @@ df_columns = {"statement_date": None,
               "service_to": None,
               "wifi_service_charge": pd.Series(dtype='float'),
               "spectrum_internet_charge": pd.Series(dtype='float'),
+              "promo_applied": pd.Series(dtype='bool'),
               "promo_discount": pd.Series(dtype='float'),
               "taxes": pd.Series(dtype='float'),
-              "total": pd.Series(dtype='float')}
+              "computed_total": pd.Series(dtype='float'),
+              "printed_total": pd.Series(dtype='float'),
+              "due_date": None}
 df = pd.DataFrame(df_columns)
 for filename in pdf_filenames:
 
+    promo_flag = False
     print(f"Reading file {filename}")
     reader = Reader(os.path.join(pdf_dir, filename))
-    pg_1 = reader.pages[0]
-    txt = pg_1.extract_text()
-    txt_lines = txt.splitlines()
-    imp_lines = txt_lines[13:40]
 
-    service_from = txt_lines[5].split(' ')[2]
-    date_from = datetime.strptime(service_from, "%m/%d/%y").date()
-    service_to = txt_lines[5].split(' ')[4]
-    date_to = datetime.strptime(service_to, "%m/%d/%y").date()
     pg_2 = reader.pages[1]
-    txt_lines_pg_2 = pg_2.extract_text().splitlines()
-    wifi_service_charge = float(txt_lines_pg_2[14].split(' ')[-1])
-    spectrum_internet_charge = float(txt_lines_pg_2[15].split(' ')[-1])
-    promotional_discount_charge = float(txt_lines_pg_2[16].split(' ')[-1])
-    taxes = float(txt_lines_pg_2[22].split(' ')[-1][1:])
-    total_due = round(wifi_service_charge + spectrum_internet_charge + promotional_discount_charge + taxes, 2)
-    temp_dict = {"statement_date": date_from,
-                 "service_from": date_from,
-                 "service_to": date_to,
-                 "wifi_service_charge": wifi_service_charge,
-                 "spectrum_internet_charge": spectrum_internet_charge,
-                 "promo_discount": promotional_discount_charge,
-                 "taxes": taxes,
-                 "total": total_due}
-    temp_df = pd.DataFrame(temp_dict, index=[0])
-    df = pd.concat([df, temp_df], axis=0, ignore_index=True)
-    df.to_excel("output.xlsx")
+    pg_2_txt = pg_2.extract_text()
+    service_date_regex = re.compile(r"service from (\d\d/\d\d/\d\d) through (\d\d/\d\d/\d\d)", re.IGNORECASE)
+    mo = service_date_regex.search(pg_2_txt)
+    service_start_date = datetime.strptime(mo.group(1), "%m/%d/%y").date()
+    service_end_date = datetime.strptime(mo.group(2), "%m/%d/%y").date()
 
+    wifi_service_regex = re.compile(r"wifi service (\d+.\d\d)", re.IGNORECASE)
+    mo = wifi_service_regex.search(pg_2_txt)
+    wifi_service_charge = float(mo.group(1))
+
+    spectrum_internet_regex = re.compile(r"spectrum internet (\d+.\d\d)", re.IGNORECASE)
+    mo = spectrum_internet_regex.search(pg_2_txt)
+    spectrum_internet_charge = float(mo.group(1))
+
+    promotional_discount_regex = re.compile(r"promotional discount (-\d+.\d\d)", re.IGNORECASE)
+    mo = promotional_discount_regex.search(pg_2_txt)
+    promotional_discount_value = float(mo.group(1))
+    if mo:
+        promo_flag = True
+        promo_discount_expiry_regex = re.compile(r"your promotional price will expire on (\d\d/\d\d/\d\d)", re.IGNORECASE)
+        mo = promo_discount_expiry_regex.search(pg_2_txt)
+        promo_discount_expiry_date = datetime.strptime(mo.group(1), "%m/%d/%y").date()
+
+    taxes_regex = re.compile(r"taxes, fees and charges total \$(\d+.\d\d)", re.IGNORECASE)
+    mo = taxes_regex.search(pg_2_txt)
+    taxes_value = float(mo.group(1))
+
+    due_date_regex = re.compile(r"total due by\s+(\d\d/\d\d/\d\d)\s+\$(\d+.\d\d)", re.IGNORECASE)
+    mo = due_date_regex.search(pg_2_txt)
+    due_date = datetime.strptime(mo.group(1), "%m/%d/%y").date()
+    printed_total = float(mo.group(2))
+
+    computed_total = wifi_service_charge + spectrum_internet_charge + taxes_value
+    if promo_flag:
+        computed_total += promotional_discount_value
+
+    temp = {"statement_date": service_start_date,
+            "service_from": service_start_date,
+            "service_to": service_end_date,
+            "wifi_service_charge": wifi_service_charge,
+            "spectrum_internet_charge": spectrum_internet_charge,
+            "promo_applied": promo_flag,
+            "promo_discount": promotional_discount_value,
+            "taxes": taxes_value,
+            "computed_total": computed_total,
+            "printed_total": printed_total,
+            "due_date": due_date
+            }
+    temp_df = pd.DataFrame(temp, index=[0])
+
+    df = pd.concat([df, temp_df], axis=0, ignore_index=True)
+
+df.sort_values(by="statement_date", ascending=True, inplace=True)
+df.set_index(pd.Series(range(1, len(pdf_filenames)+1)), inplace=True)
+df.to_excel("output.xlsx")
 print(df)
 
